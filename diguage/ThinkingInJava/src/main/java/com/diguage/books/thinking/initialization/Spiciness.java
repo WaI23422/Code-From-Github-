759f56d3a6e50c6531ebd284745e81a085e27beb
WaI23422 <124081647+WaI23422@users.noreply.github.com>
2023-09-10 14:36:21 +0700

    Add files via upload

---
+++ diguage/ThinkingInJava/src/main/java/com/diguage/books/thinking/initialization/Spiciness.java
@@ -0,0 +1,6 @@
+package com.diguage.books.thinking.initialization;
+
+//: initialization/Spiciness.java
+public enum Spiciness {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+} ///:~
---
+++ diguage/ThinkingInJava/src/main/java/com/diguage/books/thinking/initialization/Spoon.java
@@ -0,0 +1,10 @@
+package com.diguage.books.thinking.initialization;
+
+//: initialization/Spoon.java
+public class Spoon {
+    static int i;
+
+    static {
+        i = 47;
+    }
+} ///:~
